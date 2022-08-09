985f7c9ac9c95231e8d333d385c0280f59810fa5
Guilherme <guilhermerg1234@gmail.com>
2022-08-09 10:13:47 -0300

    Req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word1 = "Python"
+    word2 = "Javascript"
+
+    assert count_ocurrences(path, word1)
+    assert count_ocurrences(path, word2)
