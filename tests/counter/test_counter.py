b55f6445b1e3c08fa12f35f06b8adb4f5290a028
Guilherme <guilhermerg1234@gmail.com>
2022-08-09 11:10:34 -0300

    Req10 refactured

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -3,8 +3,7 @@ from src.counter import count_ocurrences
 
 def test_counter():
     path = "src/jobs.csv"
-    word1 = "Python"
+    word = "Python"
     word2 = "Javascript"
-
-    assert count_ocurrences(path, word1)
-    assert count_ocurrences(path, word2)
+    assert count_ocurrences(path, word) == 1639
+    assert count_ocurrences(path, word2) == 122
